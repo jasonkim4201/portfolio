eecb57f0661ae2b060780e273c81649650921b48
Jason Kim <jasonkim4201@gmail.com>
2019-04-23 15:28:51 -0400

    added basic layout for contact page

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -3,12 +3,49 @@
 // Well for starters I should add basic contact information here and links to my github page and contact info
 
 // Maybe add a form here to leave comments or whatever. Maybe make a mongo database to store in all the comments. Or SQL hmm....
-  // We'll see. last thing i want is to get spammed but i can always look into adding spam filters and what not
+// We'll see. last thing i want is to get spammed but i can always look into adding spam filters and what not
 
-  import React from "react";
+import React from "react";
 
-  const Contact = (props) => (
-    <h1>MY CONTACT INFO OKAY PLS REMEMBER</h1>
-  )
+const Contact = (props) => (
+  <section className="contact">
+    <div className="contact__container">
+      <div className="contact__text u-center-text">
+      <h1 className="u-margin-bottom-small">Contact me!</h1>
+      <p className="u-margin-bottom-medium">Also feel free to browse through my Github and LinkedIn page!</p>
+    </div>
 
-  export default Contact;
+      <div className="contact__card">
+
+        <div className="contact__info u-center-text">
+          <div className="contact__methods">
+            <i className="contact__icon far fa-envelope"></i>
+            <p>jasonkim4201@gmail.com</p>
+          </div>
+
+          <div className="contact__methods">
+            <i className="contact__icon fab fa-github"></i>
+            <p>Github</p>
+          </div>
+
+          <div className="contact__methods">
+            <i className="contact__icon fab fa-linkedin"></i>
+            <p>LinkedIn</p>
+          </div>
+          <div className="contact__methods">
+            <i className="contact__icon fas fa-phone"></i>
+            <p>(856)316-2181</p>
+          </div>
+
+
+        </div>
+
+
+
+      </div>
+
+    </div>
+  </section>
+)
+
+export default Contact;
