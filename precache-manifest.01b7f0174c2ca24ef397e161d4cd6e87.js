3f4b14730679bfef038065fc6aa1fa27b9c2a005
Jason Kim <jasonkim4201@gmail.com>
2019-04-29 05:11:32 -0400

    Updates

---
+++ precache-manifest.01b7f0174c2ca24ef397e161d4cd6e87.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "177764eadf43efc393ebb98329a774b2",
+    "url": "/portfolio/static/media/linea-basic-10.177764ea.svg"
+  },
+  {
+    "revision": "a9c8a1f14e132ca6f625",
+    "url": "/portfolio/static/css/main.ae2bc201.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "c689b7881e69e3618c1f",
+    "url": "/portfolio/static/js/2.41cbb737.chunk.js"
+  },
+  {
+    "revision": "65862e9c711c7c029346e1ac72c1959b",
+    "url": "/portfolio/static/media/pic.65862e9c.jpg"
+  },
+  {
+    "revision": "61f3a8a387a64abfc0535cd0813cd71a",
+    "url": "/portfolio/static/media/linea-basic-10.61f3a8a3.woff"
+  },
+  {
+    "revision": "e4de528c00da0c081906c3caea289f0f",
+    "url": "/portfolio/static/media/linea-basic-10.e4de528c.eot"
+  },
+  {
+    "revision": "e6763f008205c55db623cdf68bbd581f",
+    "url": "/portfolio/static/media/linea-basic-10.e6763f00.ttf"
+  },
+  {
+    "revision": "a9c8a1f14e132ca6f625",
+    "url": "/portfolio/static/js/main.53fd44e2.chunk.js"
+  },
+  {
+    "revision": "b16556b48d6daa9f0d26a16638f683d1",
+    "url": "/portfolio/static/media/big-burger.b16556b4.png"
+  },
+  {
+    "revision": "7f518e7f46223060e634d53e6258a868",
+    "url": "/portfolio/static/media/depositphotos_52458995-stock-photo-global-finance.7f518e7f.jpg"
+  },
+  {
+    "revision": "0bc21b12a8cdf295c5aa6f11bb223c5c",
+    "url": "/portfolio/static/media/cold-liquid-bar-foam-celebration-beverage-495781-pxhere.com.0bc21b12.jpg"
+  },
+  {
+    "revision": "6b07bea7493d4635d77a7946cfb04637",
+    "url": "/portfolio/static/media/lobster-nebula-1920628_1920.6b07bea7.jpg"
+  },
+  {
+    "revision": "d8631c5e3e343c2d962091d98add4e88",
+    "url": "/portfolio/static/media/blur-book-stack-books-590493.d8631c5e.jpg"
+  },
+  {
+    "revision": "15148e63fa4e6f3cdedfdfa02a8438af",
+    "url": "/portfolio/static/media/kelly-kiernan-776658-unsplash.15148e63.jpg"
+  },
+  {
+    "revision": "35fd5f335c4a029b50b112d79f4a39e3",
+    "url": "/portfolio/static/media/alcohol-alcoholism-ale-background-bar-beer-1457325-pxhere.com.35fd5f33.jpg"
+  },
+  {
+    "revision": "6e3e496915c8643bda67e0f18a715c36",
+    "url": "/portfolio/index.html"
+  }
+];
