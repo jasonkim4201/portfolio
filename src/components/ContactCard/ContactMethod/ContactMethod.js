49537e2f317d4ab09137bf520dc26cc394561d31
Jason Kim <jasonkim4201@gmail.com>
2019-04-29 04:54:18 -0400

    added more responsive features

--- src/components/ContactCard/ContactMethod/ContactMethod.js
+++ src/components/ContactCard/ContactMethod/ContactMethod.js
@@ -4,8 +4,9 @@ const ContactMethod = (props) => (
     <div className="contact__methods">
       <a href={props.link} className="contact__link">
         <i className={props.classProp}></i>
+        <p>{props.method}</p>
       </a>
-      <p>{props.method}</p>
+      
     </div>
   );
 
